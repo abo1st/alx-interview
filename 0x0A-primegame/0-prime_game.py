191be2c8da1f736e88bd09a1ecfc0054ce5ac636
abo1st <abo1st001@gmail.com>
2024-09-05 05:34:19 -0700

    0-prime_game.py

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""0. Prime Game - The game between Maria and Ben"""
+
+
+def isWinner(x, nums):
+    """x - rounds
+    nums - numbers list
+    """
+    if x <= 0 or nums is None:
+        return None
+    if x != len(nums):
+        return None
+
+    ben = 0
+    maria = 0
+
+    a = [1 for x in range(sorted(nums)[-1] + 1)]
+    a[0], a[1] = 0, 0
+    for iz in range(2, len(a)):
+        rm_multiples(a, iz)
+
+    for iz in nums:
+        if sum(a[0:iz + 1]) % 2 == 0:
+            ben += 1
+        else:
+            maria += 1
+    if ben > maria:
+        return "Ben"
+    if maria > ben:
+        return "Maria"
+    return None
+
+
+def rm_multiples(ls, x):
+    """removes multiple
+    of primes
+    """
+    for iz in range(2, len(ls)):
+        try:
+            ls[iz * x] = 0
+        except (ValueError, IndexError):
+            break
